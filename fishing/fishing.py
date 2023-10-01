bd517af50a96ee725f356dae5347c93177da6773
Dchamel <dchamel@yandex.ru>
2023-10-01 23:56:35 +0400

    Succeed ver  - ver 1.0  - add day/night float screen

--- fishing/fishing.py
+++ fishing/fishing.py
@@ -2,40 +2,67 @@ import time
 
 import cv2
 import numpy as np
+import pyautogui
 from PIL import ImageGrab
 
-bobber = cv2.imread('bobber1.png')
-bw, bh = bobber.shape[1::-1]
-print(bobber)
-bobber_gray = cv2.cvtColor(bobber, cv2.COLOR_BGR2GRAY)
+# float = cv2.imread('float_day.png')
+float = cv2.imread('float_night.png')
+bw, bh = float.shape[1::-1]
+float_gray = cv2.cvtColor(float, cv2.COLOR_BGR2GRAY)
 
 average = [0, ]
-for q in range(20):
-    print(f'Step: {q}')
-    main_scrshot = ImageGrab.grab(bbox=(260, 0, 1530, 420))
-    # main_scrshot.save('main_scrshot.png')
+pyautogui.moveTo(1465, 976)
+pyautogui.mouseDown()
+time.sleep(0.1)
+pyautogui.mouseUp()
+for q in range(1000):
+    try:
+        time.sleep(1)
+        pyautogui.moveTo(1465, 976)
+        pyautogui.mouseDown()
+        time.sleep(0.2)
+        pyautogui.mouseUp()
+        time.sleep(2)
+        print(f'Step: {q}')
+        main_scrshot = ImageGrab.grab(bbox=(260, 0, 1530, 420))
+        main_scrshot.save('main_scrshot.png')
 
-    main_rgb = cv2.imread('main_scrshot.png')
-    main_gray = cv2.cvtColor(main_rgb, cv2.COLOR_BGR2GRAY)
+        main_rgb = cv2.imread('main_scrshot.png')
+        main_gray = cv2.cvtColor(main_rgb, cv2.COLOR_BGR2GRAY)
 
-    res = cv2.matchTemplate(main_gray, bobber_gray, cv2.TM_CCOEFF_NORMED)
-    loc = np.where(res >= 0.7)
-    for i in range(10):
-        try:
-            bobber_scrshot = ImageGrab.grab(bbox=(x, y, x - bw, y - bh))
-            time.sleep(0.1)
-            bobber_scrshot.save('bobber_scrshot.png')
-            bobber_move = np.mean(bobber_scrshot)
-            fish_biting = average[-1] - bobber_move
-            # print(fish_biting)
-            if fish_biting >= 1:
-                print(True)
-                time.sleep(0.4)
-                break
-            average.append(bobber_move)
-        except:
-            for coord in zip(*loc[::-1]):
-                x = int(coord[0])
-                y = int(coord[1])
+        res = cv2.matchTemplate(main_gray, float_gray, cv2.TM_CCOEFF_NORMED)
+        loc = np.where(res >= 0.7)
+        for i in range(70):
+            try:
+                float_scrshot = ImageGrab.grab(bbox=(x + 260, y, x + 260 + bw, y + bh))
+                time.sleep(0.1)
+                float_scrshot.save('float_scrshot.png')
+                float_move = np.mean(float_scrshot)
+                fish_biting = average[-1] - float_move
+                print(fish_biting)
+                if fish_biting >= 2:
+                    pyautogui.moveTo(x + 260 + 15, y + 17)
+                    pyautogui.mouseDown()
+                    time.sleep(0.2)
+                    pyautogui.mouseUp()
+                    pyautogui.mouseDown(button='right')
+                    time.sleep(0.2)
+                    pyautogui.mouseUp(button='right')
+                    break
+                average.append(float_move)
+            except:
+                for coord in zip(*loc[::-1]):
+                    x = int(coord[0])
+                    y = int(coord[1])
                 print(x, y)
-            time.sleep(0.1)
+                time.sleep(0.2)
+        pyautogui.moveTo(100, 100)
+        try:
+            del x
+            del y
+        except:
+            pass
+        average = [0, ]
+        time.sleep(1)
+    except:
+        continue
